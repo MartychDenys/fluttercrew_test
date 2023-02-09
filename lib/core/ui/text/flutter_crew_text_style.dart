eb69a89ac7f376919c476831a8676a7d985187ee
denysmartych <martych88@gmail.com>
2023-02-09 22:04:41 +0200

    Flutter crew test

---
+++ lib/core/ui/text/flutter_crew_text_style.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/widgets.dart';
+
+const _basicTextStyle = TextStyle(
+  overflow: TextOverflow.ellipsis,
+);
+
+final fTextH0Style = _basicTextStyle.copyWith(
+  fontSize: 32.0,
+  fontWeight: FontWeight.w600,
+  height: 1.07,
+  fontFamily: 'Roboto',
+);
+
+final fTextH1Style = _basicTextStyle.copyWith(
+  fontSize: 32.0,
+  fontWeight: FontWeight.w900,
+  fontFamily: 'Roboto',
+);
+
+final fTextH2Style = _basicTextStyle.copyWith(
+  fontSize: 18.0,
+  fontWeight: FontWeight.w700,
+  height: 1.07,
+  fontFamily: 'Roboto',
+);
+
+final fBodyText0Style = _basicTextStyle.copyWith(
+  fontSize: 16.0,
+  fontWeight: FontWeight.w600,
+  height: 1.50,
+  fontFamily: 'Roboto',
+);
+
+final fBodyText1Style = _basicTextStyle.copyWith(
+  fontSize: 16.0,
+  fontWeight: FontWeight.w500,
+  height: 1.50,
+  fontFamily: 'Roboto',
+);
+
+final fBodyText2Style = _basicTextStyle.copyWith(
+  fontSize: 16.0,
+  fontWeight: FontWeight.w400,
+  height: 1.50,
+  fontFamily: 'Roboto',
+);
+
+final fBodyText3Style = _basicTextStyle.copyWith(
+  fontSize: 12.0,
+  fontWeight: FontWeight.w500,
+  fontFamily: 'Roboto',
+);
